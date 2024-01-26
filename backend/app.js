df6966ab02d669500f7b170724eadfd4a26906de
Leighton <l.k.buchanancates@gmail.com>
2024-01-25 21:59:18 -0500

    creates json file locally, grabs plant names from api, hides api key in .env file

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,29 @@
-const apiKey = "sk-q5Nf653a5334aa6dc2721";
-// 1. fetch api, get same data
-// 2. put in jsonfile (stringify)
+require("dotenv").config();
+const fetch = require("node-fetch");
+const fs = require("fs");
 
+const apiKey = process.env.PERENUAL_API_KEY;
 const plantArray = [];
-const pageNums = [1, 2, 3];
-for (const pageNum of pageNums) {
-  const fetch = require("node-fetch");
-  fetch(`https://perenual.com/api/species-list?key=${apiKey}&page=${pageNum}`)
-    .then((res) => res.json())
-    .then((json) => {
-      plantArray.push(json.data);
-      console.log(plantArray.flat());
-    });
+const pageNums = [1, 2, 3, 4, 5];
+
+async function getPlantNames() {
+  for (const pageNum of pageNums) {
+    const response = await fetch(
+      `https://perenual.com/api/species-list?key=${apiKey}&page=${pageNum}`
+    );
+    const json = await response.json();
+    const plantNames = json.data.map((plant) => plant.common_name);
+    plantArray.push(plantNames);
+  }
+  const plantTitles = plantArray.flat();
+  return plantTitles;
 }
 
-// dropdown based on input, suggests based on input
-
-// on page load, load all plant names
-// as user types (onKeyDown), compare input string to match plant names
-// allow keyboard arrow to navigate and enter key to select
-
-// const fetch = require("node-fetch");
-// const apiUrl = "https://dog.ceo/api/breeds/list/all";
+getPlantNames().then((names) => {
+  const json = JSON.stringify(names);
+  const callback = () =>
+    console.log(
+      `Successfully wrote ${names.length} plant names to JSON file!!`
+    );
+  fs.writeFile("data.json", json, "utf8", callback);
+});
---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+// function findMatches(wordToMatch, plantArray) {
+//   return plantArray.filter((plantName) => {
+//     //plant name matches what was searched
+//     const regex = new RegExp(wordToMatch, "gi");
+//     return plantName.plant.match(regex);
+//   });
+// }
+
+console.log(plantNames);
