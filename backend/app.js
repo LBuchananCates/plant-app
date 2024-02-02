795c3c86e2f67d99e59d85b906bdcadd16af6db6
Leighton <l.k.buchanancates@gmail.com>
2024-02-02 12:41:04 -0500

    imports & exports live server to gain access to local files

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,41 @@
+const { liveServer, params } = require("./live-server.js");
 require("dotenv").config();
 const fetch = require("node-fetch");
 const fs = require("fs");
 
-const apiKey = process.env.PERENUAL_API_KEY;
-const plantArray = [];
-const pageNums = [1, 2, 3, 4, 5];
+console.log(liveServer.params);
+liveServer.start(params);
 
-async function getPlantNames() {
-  for (const pageNum of pageNums) {
-    const response = await fetch(
-      `https://perenual.com/api/species-list?key=${apiKey}&page=${pageNum}`
-    );
-    const json = await response.json();
+// const apiKey = process.env.PERENUAL_API_KEY;
+// const plantArray = [];
+// const pageNums = [1, 2, 3, 4, 5];
 
-    // return one array of objects
-    // ** map returns an array, isn't destructive
-    // map over data
-    // write function inside of map that returns smth that all goes inside array
-    // inside map function, return an object
-    const plants = json.data.map((plant) => {
-      console.log(plant);
-      // returning object
-      return {
-        commonName: plant.common_name,
-        scientificName: plant.scientific_name[0],
-        watering: plant.watering,
-        sunlight: plant.sunlight,
-      };
-    });
-    plantArray.push(plants);
-  }
-  return plantArray.flat();
-}
+// async function getPlantNames() {
+//   for (const pageNum of pageNums) {
+//     const response = await fetch(
+//       `https://perenual.com/api/species-list?key=${apiKey}&page=${pageNum}`
+//     );
+//     const json = await response.json();
 
-getPlantNames().then((names) => {
-  const json = JSON.stringify(names);
-  const callback = () =>
-    console.log(
-      `Successfully wrote ${names.length} plant names to JSON file!!`
-    );
-  fs.writeFile("data.json", json, "utf8", callback);
-});
+//     const plants = json.data.map((plant) => {
+//       console.log(plant);
+//       return {
+//         commonName: plant.common_name,
+//         scientificName: plant.scientific_name[0],
+//         watering: plant.watering,
+//         sunlight: plant.sunlight,
+//       };
+//     });
+//     plantArray.push(plants);
+//   }
+//   return plantArray.flat();
+// }
+
+// getPlantNames().then((names) => {
+//   const json = JSON.stringify(names);
+//   const callback = () =>
+//     console.log(
+//       `Successfully wrote ${names.length} plant names to JSON file!!`
+//     );
+//   fs.writeFile("data.json", json, "utf8", callback);
+// });
--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,37 @@
-// function findMatches(wordToMatch, plantArray) {
-//   return plantArray.filter((plantName) => {
-//     //plant name matches what was searched
-//     const regex = new RegExp(wordToMatch, "gi");
-//     return plantName.plant.match(regex);
-//   });
-// }
+// turn data into array
+const endpoint = "../backend/data.json";
 
-console.log(plantNames);
+// fetch json data
+fetch(endpoint)
+  .then((res) => res.json())
+  // .then((data) => plants.push(...data))
+  .then((data) => console.log(data));
+
+// define findMatches function
+function findMatches(wordToMatch, plantArray) {
+  return plantArray.filter((plantName) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return plantName.plant.match(regex);
+  });
+}
+
+function createBulletList() {
+  // grab first 5 plants from data
+  // for each plant, =
+  // create li with plant names use innerText
+  // put inside ul
+  // put ul in document.body
+}
+
+// display matches from previous funciton
+function displayMatches() {
+  //   const matchArray = findMatches(this.value, plants);
+  console.log(this.value);
+}
+
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+// as new information typed, update matches WORKS
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
