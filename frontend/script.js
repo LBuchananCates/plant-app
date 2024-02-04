b1091dbb892ef4806fb35be7dab4db6afd95311c
Leighton <l.k.buchanancates@gmail.com>
2024-02-04 15:33:08 -0500

    plants matching input value appear on frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,47 +1,73 @@
 const endpoint = "../backend/data.json";
 
+let plants = [];
+
 // fetch json data
 fetch(endpoint)
   .then((res) => res.json())
   .then((data) => {
+    plants = data;
     createPlantListing(data);
   });
 
-// define findMatches function
-function findMatches(wordToMatch, plantArray) {
-  return plantArray.filter((plantName) => {
-    const regex = new RegExp(wordToMatch, "gi");
-    return plantName.plant.match(regex);
+// grab user's input
+function myInput() {
+  // grab input element
+  let input = document.getElementById("search");
+  // grab value of input element
+  let inputText = input.value;
+  input.addEventListener("keyup", (e) => {
+    // match inputText by running findMatches
+    findMatches(inputText, plants);
   });
 }
 
-let plants = [];
-
-// create plant listing
-function createPlantListing(info) {
-  console.log(info);
-  // create li with plant names use innerText
-  const plantLi = document.createElement("li");
-  plantLi.innerText = info[(0, 1)].commonName;
-  plantLi.className = "plant-listing";
-  // create ul
-  const newUl = document.createElement("ul");
-  // append plantLi to newUl
-  newUl.append(plantLi);
-  // append newUl to form in main
-  const form = document.getElementById("search-form");
-  form.append(newUl);
+// define findMatches to match input value with plants array
+function findMatches(wordToMatch, plants) {
+  return plants.filter((plant) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return plant.commonName.match(regex);
+  });
 }
 
 // display matches from previous funciton
 function displayMatches() {
-  //   const matchArray = findMatches(this.value, plants);
-  console.log(this.value);
+  let input = document.getElementById("search");
+  let inputText = input.value;
+  const matchPlantArray = findMatches(inputText, plants);
+
+  function createPlantListing(info) {
+    const ul = document.createElement("ul");
+    info.forEach((plant) => {
+      const li = document.createElement("li");
+      li.innerText = plant.commonName;
+      li.className = "plant-listing";
+      ul.className = "suggestions";
+      ul.appendChild(li);
+      // if no match, display nothing
+    });
+    const form = document.getElementById("search-form");
+    form.appendChild(ul);
+  }
+  createPlantListing(matchPlantArray);
+  console.log(matchPlantArray);
+  return matchPlantArray;
 }
 
 const searchInput = document.querySelector(".search");
-const suggestions = document.querySelector(".suggestions");
 
 // as new information typed, update matches WORKS
 searchInput.addEventListener("change", displayMatches);
-searchInput.addEventListener("keyup", displayMatches);
+
+// function createPlantListing(info) {
+//   const ul = document.createElement("ul");
+//   info.forEach((plant) => {
+//     const li = document.createElement("li");
+//     li.innerText = plant.commonName;
+//     li.className = "plant-listing";
+//     ul.className = "suggestions";
+//     ul.appendChild(li);
+//   });
+//   const form = document.getElementById("search-form");
+//   form.appendChild(ul);
+// }
