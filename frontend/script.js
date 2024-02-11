e683607ff22b482d374094709719aaa7256afb6d
Leighton <l.k.buchanancates@gmail.com>
2024-02-11 12:21:41 -0500

    appends 'no results found' to form if input value doesn't match any plants

--- frontend/script.js
+++ frontend/script.js
@@ -7,14 +7,20 @@ fetch(endpoint)
   .then((res) => res.json())
   .then((data) => {
     plants = data;
-    createPlantListing(data);
   });
+// define findMatches to match input value with plants array WORKS
+function findMatches(input) {
+  return plants.filter((plant) => {
+    const regex = new RegExp(input, "gi");
+    return plant.commonName.match(regex);
+  });
+}
 
 // grab user's input
 function myInput() {
   // grab input element
   let input = document.getElementById("search");
-  // grab value of input element
+  // grab value of input element SOMETHING WRONG
   let inputText = input.value;
   input.addEventListener("keyup", (e) => {
     // match inputText by running findMatches
@@ -22,52 +28,58 @@ function myInput() {
   });
 }
 
-// define findMatches to match input value with plants array
-function findMatches(wordToMatch, plants) {
-  return plants.filter((plant) => {
-    const regex = new RegExp(wordToMatch, "gi");
-    return plant.commonName.match(regex);
-  });
-}
-
-// display matches from previous funciton
-function displayMatches() {
-  let input = document.getElementById("search");
-  let inputText = input.value;
-  const matchPlantArray = findMatches(inputText, plants);
-
-  function createPlantListing(info) {
-    const ul = document.createElement("ul");
-    info.forEach((plant) => {
-      const li = document.createElement("li");
-      li.innerText = plant.commonName;
-      li.className = "plant-listing";
-      ul.className = "suggestions";
-      ul.appendChild(li);
-      // if no match, display nothing
-    });
-    const form = document.getElementById("search-form");
-    form.appendChild(ul);
-  }
-  createPlantListing(matchPlantArray);
-  console.log(matchPlantArray);
-  return matchPlantArray;
-}
-
 const searchInput = document.querySelector(".search");
+// 5 sec wait works
+let timer,
+  timeoutVal = 5000; // time it takes to wait
+const input = document.querySelector("#search-input");
+input.addEventListener("keyup", handleKeyUp);
+function handleKeyUp(e) {
+  window.clearTimeout(timer);
+  timer = window.setTimeout(() => {
+    console.log("all done typing");
+  }, timeoutVal);
+}
 
 // as new information typed, update matches WORKS
-searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("change", (event) => {
+  // how to get input from event
+  const matchingPlants = findMatches(event.target.value);
+  // display matchingPlants in ul
+  displayPlantListings(matchingPlants);
+  // if no matching plants in plantarray
+  if (matchingPlants.length == 0) {
+    // return div below search input "No searches found"
+    const noResults = document.createElement("div");
+    noResults.innerText = "No results found";
+    noResults.setAttribute("class", "no-results-found");
+    // append noresults as child to form tag so
+    // grab form tag
+    const form = document.querySelector("#search-form");
+    form.appendChild(noResults);
+  }
+  // when user changes input, remove list and replace with updated input.value
+  console.log(matchingPlants);
+});
 
-// function createPlantListing(info) {
-//   const ul = document.createElement("ul");
-//   info.forEach((plant) => {
-//     const li = document.createElement("li");
-//     li.innerText = plant.commonName;
-//     li.className = "plant-listing";
-//     ul.className = "suggestions";
-//     ul.appendChild(li);
-//   });
-//   const form = document.getElementById("search-form");
-//   form.appendChild(ul);
-// }
+function displayPlantListings(plants) {
+  plants.forEach((plant) => {
+    const ul = document.querySelector(".suggestions");
+
+    if (ul) {
+      // if existing ul
+      const searchForm = document.querySelector("#search-form");
+      searchForm.appendChild(ul);
+      // then delete existing ul
+    } else {
+      const newUl = document.createElement("ul");
+      newUl.setAttribute("id", "suggestions-ul"); // <ul id="suggestions-ul"> </ul>
+      // and add li element to newUl
+      const li = document.createElement("li");
+      li.innerText = plant.commonName; // <li class="plant-listing"> {plant.commonName}</li>
+      li.className = "plant-listing";
+      newUl.appendChild(li);
+      // searchForm.appendChild(newUl);
+    }
+  });
+}
