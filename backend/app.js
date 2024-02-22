0728b7a1843f42d452e3b1361d5ae2252aac20ba
Leighton <l.k.buchanancates@gmail.com>
2024-02-22 13:29:08 -0500

    finalizes backend code

--- backend/app.js
+++ backend/app.js
@@ -6,36 +6,36 @@ const fs = require("fs");
 console.log(liveServer.params);
 liveServer.start(params);
 
-// const apiKey = process.env.PERENUAL_API_KEY;
-// const plantArray = [];
-// const pageNums = [1, 2, 3, 4, 5];
+const apiKey = process.env.PERENUAL_API_KEY;
+const plantArray = [];
+const pageNums = [1, 2, 3, 4, 5];
 
-// async function getPlantNames() {
-//   for (const pageNum of pageNums) {
-//     const response = await fetch(
-//       `https://perenual.com/api/species-list?key=${apiKey}&page=${pageNum}`
-//     );
-//     const json = await response.json();
+async function getPlantNames() {
+  for (const pageNum of pageNums) {
+    const response = await fetch(
+      `https://perenual.com/api/species-list?key=${apiKey}&page=${pageNum}`
+    );
+    const json = await response.json();
 
-//     const plants = json.data.map((plant) => {
-//       console.log(plant);
-//       return {
-//         commonName: plant.common_name,
-//         scientificName: plant.scientific_name[0],
-//         watering: plant.watering,
-//         sunlight: plant.sunlight,
-//       };
-//     });
-//     plantArray.push(plants);
-//   }
-//   return plantArray.flat();
-// }
+    const plants = json.data.map((plant) => {
+      console.log(plant);
+      return {
+        commonName: plant.common_name,
+        scientificName: plant.scientific_name[0],
+        watering: plant.watering,
+        sunlight: plant.sunlight,
+      };
+    });
+    plantArray.push(plants);
+  }
+  return plantArray.flat();
+}
 
-// getPlantNames().then((names) => {
-//   const json = JSON.stringify(names);
-//   const callback = () =>
-//     console.log(
-//       `Successfully wrote ${names.length} plant names to JSON file!!`
-//     );
-//   fs.writeFile("data.json", json, "utf8", callback);
-// });
+getPlantNames().then((names) => {
+  const json = JSON.stringify(names);
+  const callback = () =>
+    console.log(
+      `Successfully wrote ${names.length} plant names to JSON file!!`
+    );
+  fs.writeFile("data.json", json, "utf8", callback);
+});
