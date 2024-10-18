075dc951f8aa58bc110a65d54ebf47f6059fab67
Leighton <l.k.buchanancates@gmail.com>
2024-10-18 18:57:22 +0900

    adds styles to indiv. plant traits

--- frontend/plant.js
+++ frontend/plant.js
@@ -11,13 +11,20 @@ main.appendChild(heading);
 // 2. put plant scientific name into p
 const scientificNameParagraph = document.createElement("p");
 const scientificName = params.get("scientificName");
+scientificNameParagraph.style.fontStyle = "italic";
 scientificNameParagraph.innerText = scientificName;
 main.appendChild(scientificNameParagraph);
 
 // 3. put plant watering into p
+const wateringParagraph = document.createElement("p");
+const watering = params.get("watering");
+wateringParagraph.style.color = "blue";
+wateringParagraph.innerText = `Watering schedule: ${watering}`;
+main.appendChild(wateringParagraph);
+
 // 4. put plant sunlight into p
 const sunlightParagraph = document.createElement("p");
 let sunlight = params.getAll("sunlight");
-sunlight = sunlight.join(" ");
-sunlightParagraph.innerText = sunlight;
+sunlight = sunlight.join(", ");
+sunlightParagraph.innerText = `Sunlight: ${sunlight}`;
 main.appendChild(sunlightParagraph);
