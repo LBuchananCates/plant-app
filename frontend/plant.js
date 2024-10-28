dc12c31eb8831ccd16023789dc6256b00b2f0643
Leighton <l.k.buchanancates@gmail.com>
2024-10-28 23:31:12 +0900

    adds more styles with js for plant-template page; event listener for return button works

--- frontend/plant.js
+++ frontend/plant.js
@@ -1,38 +1,49 @@
 const params = new URLSearchParams(document.location.search);
 const main = document.querySelector("main");
 
-// to do
-// 1. put plant name into h1
+// RETURN BUTTON
+const returnBtn = document.getElementById("btn__return");
+returnBtn.style.margin = "40px 60px";
+// when click button, redirect to index.html
+returnBtn.addEventListener("click", function (event) {
+  console.log("hello world");
+});
+
+// PLANT HEADING
 const heading = document.createElement("h1");
 heading.className = "heading__plant-name";
 // styles
+heading.style.fontSize = "36px";
 heading.style.color = "#957498";
-heading.style.padding = "30px 60px";
-
+heading.style.padding = "10px 60px";
+// assign classname
 const plantName = params.get("plantName");
 heading.innerText = plantName;
 main.appendChild(heading);
 
-// 2. put plant scientific name into p
+// LATIN NAME
 const scientificNameParagraph = document.createElement("p");
+// styling
+scientificNameParagraph.style.fontStyle = "italic";
 scientificNameParagraph.style.padding = "0px 60px";
 // assign class name
 const scientificName = params.get("scientificName");
-scientificNameParagraph.style.fontStyle = "italic";
 scientificNameParagraph.innerText = scientificName;
 main.appendChild(scientificNameParagraph);
 
-// 3. put plant watering into p
+// WATERING
 const wateringParagraph = document.createElement("p");
+// styling
+wateringParagraph.style.color = "blue";
 wateringParagraph.style.padding = "0px 60px";
 // assign class name
 const watering = params.get("watering");
-wateringParagraph.style.color = "blue";
 wateringParagraph.innerText = `Watering schedule: ${watering}`;
 main.appendChild(wateringParagraph);
 
-// 4. put plant sunlight into p
+// SUNLIGHT
 const sunlightParagraph = document.createElement("p");
+// styling
 sunlightParagraph.style.padding = "0px 60px";
 // assign class name
 let sunlight = params.getAll("sunlight");
